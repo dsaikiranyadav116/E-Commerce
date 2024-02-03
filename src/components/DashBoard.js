5b46c9ce2af4a9a201c08ca95f1d997880c29f83
Saikiran@116 <saikiranyadavdesh16@gmail.com>
2024-02-03 12:49:19 +0530

    first commit

---
+++ src/components/DashBoard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Product from './Product'
+import '../App.css'
+export default function DashBoard() {
+  return (
+    <>
+        <div className='cont'>
+        <Product/>
+
+        </div>
+    </>
+  )
+}
+
